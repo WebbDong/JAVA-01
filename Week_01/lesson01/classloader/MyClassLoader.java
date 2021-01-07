ea2f92d5a09ad2e12b46c8ecf7649acccb2fa8f9
WebbDong <dwbdeveloper1991@163.com>
2021-01-07 22:57:06 +0800

    RemoteClassLoader

--- Week_01/lesson01/classloader/MyClassLoader.java
+++ Week_01/lesson01/classloader/MyClassLoader.java
@@ -11,42 +11,14 @@ import java.io.InputStream;
  *      容是一个 Hello.xlass 文件所有字节（x=255-x）处理后的文件。
  * @date 2021-01-07 11:01
  */
-public class MyClassLoader extends ClassLoader {
-
-    /**
-     * 重写 loadClass 打破双亲委派机制
-     * @param name
-     * @param resolve
-     * @return
-     * @throws ClassNotFoundException
-     */
-    @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        synchronized (getClassLoadingLock(name)) {
-            // 查看此类是否以及被加载过，如果已经加载过，直接从缓存中获取
-            Class<?> c = findLoadedClass(name);
-            if (c == null) {
-                try {
-                    // 排除 jdk 的包，自己的类才打破双亲委派机制
-                    if (!(name.startsWith("java") || name.startsWith("jdk")
-                            || name.startsWith("sun"))) {
-                        c = this.findClass(name);
-                    } else {
-                        c = this.getParent().loadClass(name);
-                    }
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            if (resolve) {
-                resolveClass(c);
-            }
-            return c;
-        }
-    }
+public class MyClassLoader extends BaseNoParentDelegationClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+        if (name == null || name == "") {
+            throw new ClassNotFoundException();
+        }
+
         String[] nameSplits = name.split("\\.");
         // 是否有文件后缀名
         boolean isNoFileSuffix = nameSplits.length == 1;
@@ -54,32 +26,20 @@ public class MyClassLoader extends ClassLoader {
             // 没有文件后缀名，默认使用 .class
             name = name + ".class";
         }
-        byte[] bytes = readClassAsBytes(name);
-        ClassCodeUtils.xlassDecode(bytes);
-        // 将文件名去掉后缀，否则会报 NoClassDefFoundError: wrong name
-        return defineClass(nameSplits[0], bytes, 0, bytes.length);
-    }
 
-    /**
-     * 读取包中指定的字节码文件，并且将流转换成 byte 数组
-     * @param name
-     * @return
-     */
-    private byte[] readClassAsBytes(String name) {
-        InputStream in = this.getSystemResourceAsStream(name);
         byte[] bytes;
-        try {
-            bytes = new byte[in.available()];
+        try (InputStream in = this.getSystemResourceAsStream(name)) {
+            if (in == null) {
+                throw new ClassNotFoundException(name);
+            }
+            bytes = ClassCodeUtils.readLocalClassAsBytes(in);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        try {
-            in.read(bytes, 0, bytes.length);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return bytes;
+        ClassCodeUtils.xlassDecode(bytes);
+        // 将文件名去掉后缀不可以有文件后缀名，否则会报 NoClassDefFoundError: wrong name
+        return defineClass(nameSplits[0], bytes, 0, bytes.length);
     }
 
     public static void main(String[] args) throws Exception {
@@ -88,7 +48,8 @@ public class MyClassLoader extends ClassLoader {
         Object obj1 = clazz1.getDeclaredConstructor().newInstance();
         clazz1.getDeclaredMethod("hello").invoke(obj1);
 
-        // 加载 Hello.class，调用 Class.forName 方法加载类，会遵循双亲委派机制，使用此方式需要打破双亲委派机制
+        // 加载 Hello.class，调用 Class.forName 方法加载类，会遵循双亲委派机制，因为使用的字节码文件放在 classpath 中
+        // 所以父类加载器会先加载到。这种情况使用此方式需要打破双亲委派机制。
         Class<?> clazz2 = Class.forName("Hello", false, new MyClassLoader());
         Object obj2 = clazz2.getDeclaredConstructor().newInstance();
         clazz2.getDeclaredMethod("hello").invoke(obj2);
--- Week_01/lesson01/util/ClassCodeUtils.java
+++ Week_01/lesson01/util/ClassCodeUtils.java
@@ -1,5 +1,11 @@
 package lesson01.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 /**
@@ -18,6 +24,62 @@ public class ClassCodeUtils {
     public static void xlassDecode(byte[] bytes) {
         IntStream.range(0, bytes.length).boxed()
                 .forEach(i -> bytes[i] = (byte) (255 - bytes[i]));
+    }
+
+    /**
+     * 读取包中指定的本地字节码文件流，并且将流转换成 byte 数组
+     * @param in
+     * @return
+     */
+    public static byte[] readLocalClassAsBytes(InputStream in) {
+        byte[] bytes;
+        try {
+            bytes = new byte[in.available()];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            in.read(bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bytes;
+    }
+
+    /**
+     * 获取网络中的字节码文件
+     * @param urlString
+     * @return
+     * @throws IOException
+     */
+    public static byte[] getRemoteClassFileAsBytes(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(1000);
+        conn.setRequestMethod("GET");
+        try (InputStream in = conn.getInputStream()) {
+            if (in == null) {
+                throw new IOException("InputStream is null");
+            }
+            // 请求获取远程字节码文件
+            List<Byte> byteList = new ArrayList<>();
+            final int BUFFER_SIZE = 1024;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int len;
+            while ((len = in.read(buffer, 0, BUFFER_SIZE)) != -1) {
+                IntStream.range(0, len).boxed().forEach(i -> byteList.add(buffer[i]));
+            }
+
+            // List<Byte> 转 byte[]
+            Object[] boxedArray = byteList.toArray();
+            len = boxedArray.length;
+            byte[] bytes = new byte[boxedArray.length];
+            for (int i = 0; i < len; i++) {
+                bytes[i] = (byte) boxedArray[i];
+            }
+            return bytes;
+        }
     }
 
 }
