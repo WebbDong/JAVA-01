18dc0f977d358035c3462b3e845fc3f0e8ae06f6
WebbDong <dwbdeveloper1991@163.com>
2021-01-23 23:59:44 +0800

    Week2作业

--- Week_02/lesson03/gc/NoRandomGCLogAnalysis.java
+++ Week_02/lesson03/gc/NoRandomGCLogAnalysis.java
@@ -1,17 +1,14 @@
 package lesson03.gc;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author Webb Dong
- * @description: 演示 GC 日志生成与分析，包含随机算法
- * @date 2021-01-22 00:49
+ * @description: 演示 GC 日志生成与分析，不使用随机算法
+ * @date 2021-01-22 14:03
  */
-public class GCLogAnalysis {
-
-    private static Random random = new Random();
+public class NoRandomGCLogAnalysis {
 
     public static void main(String[] args) {
         // 持续运行毫秒数; 可根据需要进行修改
@@ -22,48 +19,49 @@ public class GCLogAnalysis {
         long endMillis = startMillis + timeoutMillis;
         LongAdder counter = new LongAdder();
         System.out.println("正在执行...");
-        // 缓存一部分对象; 进入老年代
+        // 缓存 2000 个对象，让其进入老年代
         int cacheSize = 2000;
         Object[] cachedGarbage = new Object[cacheSize];
         // 在此时间范围内,持续循环
-        while (System.currentTimeMillis() < endMillis) {
-            // 生成垃圾对象
-            Object garbage = generateGarbage(100 * 1024);
-            counter.increment();
-            int randomIndex = random.nextInt(2 * cacheSize);
-            if (randomIndex < cacheSize) {
-                cachedGarbage[randomIndex] = garbage;
+        try {
+            while (System.currentTimeMillis() < endMillis) {
+                // 生成垃圾对象
+                Object garbage = generateGarbage(counter.intValue());
+                if (counter.intValue() < cacheSize) {
+                    cachedGarbage[counter.intValue()] = garbage;
+                }
+                counter.increment();
             }
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            System.out.println("执行异常!异常前共生成对象次数:" + counter.longValue());
+            return;
         }
         System.out.println("执行结束!共生成对象次数:" + counter.longValue());
     }
 
     /**
-     * 随机生成对象
-     * @param max
+     * 生成对象
      * @return
      */
-    private static Object generateGarbage(int max) {
-        int randomSize = random.nextInt(max);
-        int type = randomSize % 4;
+    private static Object generateGarbage(int n) {
+        final int SIZE = 40960;
+        int type = n % 4;
         Object result;
         switch (type) {
             case 0:
-                result = new int[randomSize];
+                result = new int[SIZE];
                 break;
             case 1:
-                result = new byte[randomSize];
+                result = new byte[SIZE];
                 break;
             case 2:
-                result = new double[randomSize];
+                result = new double[SIZE];
                 break;
             default:
                 StringBuilder builder = new StringBuilder();
-                String randomString = "randomString-Anything";
-                while (builder.length() < randomSize) {
-                    builder.append(randomString);
-                    builder.append(max);
-                    builder.append(randomSize);
+                for (int i = 0; i < 10; i++) {
+                    builder.append(i);
                 }
                 result = builder.toString();
                 break;
