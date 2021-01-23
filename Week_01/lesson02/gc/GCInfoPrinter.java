a07162e2e5e00588f15d65f35f060d18171976be
WebbDong <dwbdeveloper1991@163.com>
2021-01-23 11:08:07 +0800

    GCInfoPrinter

--- Week_01/lesson02/gc/GCInfoPrinter.java
+++ Week_01/lesson02/gc/GCInfoPrinter.java
@@ -12,6 +12,32 @@ import java.util.List;
  */
 public class GCInfoPrinter {
 
+    /**
+     * -XX:+UseSerialGC:
+     * GC Name:Copy, [Eden Space, Survivor Space]
+     * GC Name:MarkSweepCompact, [Eden Space, Survivor Space, Tenured Gen]
+     *
+     * -XX:+UseParNewGC: ParNew + Serial Old，这种组合已过时，不推荐使用
+     * GC Name:ParNew, [Par Eden Space, Par Survivor Space]
+     * GC Name:MarkSweepCompact, [Par Eden Space, Par Survivor Space, Tenured Gen]
+     *
+     * -XX:+UseParallelGC:
+     * GC Name:PS Scavenge, [PS Eden Space, PS Survivor Space]
+     * GC Name:PS MarkSweep, [PS Eden Space, PS Survivor Space, PS Old Gen]
+     *
+     * -XX:+UseConcMarkSweepGC:
+     * GC Name:ParNew, [Par Eden Space, Par Survivor Space]
+     * GC Name:ConcurrentMarkSweep, [Par Eden Space, Par Survivor Space, CMS Old Gen]
+     *
+     * -XX:-UseParNewGC -XX:+UseConcMarkSweepGC: Serial + CMS，这种组合已过时，不推荐使用
+     * GC Name:Copy, [Eden Space, Survivor Space]
+     * GC Name:ConcurrentMarkSweep, [Eden Space, Survivor Space, CMS Old Gen]
+     *
+     * -XX:+UseG1GC:
+     * GC Name:G1 Young Generation, [G1 Eden Space, G1 Survivor Space]
+     * GC Name:G1 Old Generation, [G1 Eden Space, G1 Survivor Space, G1 Old Gen]
+     * @param args
+     */
     public static void main(String[] args) {
         try {
             List<GarbageCollectorMXBean> gcMxBeans = ManagementFactory.getGarbageCollectorMXBeans();
