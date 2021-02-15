b3cb6633846345686f2b31afb95a91de6801f72e
Webb Dong <wangwebb1991@gmail.com>
2021-02-15 23:38:19 +0800

    写代码实现 Spring Bean 的装配

---
+++ Week_05/lesson09/beanfactory/BeanFactoryTestMain.java
@@ -0,0 +1,14 @@
+package lesson09.beanfactory;
+
+/**
+ * @author Webb Dong
+ * @description: 写代码实现 Spring Bean 的装配
+ * @date 2021-02-15 15:11
+ */
+public class BeanFactoryTestMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
