2272132dd08313e60dea8240cba3bcb49151f625
Webb Dong <wangwebb1991@gmail.com>
2021-02-15 23:31:44 +0800

    beans

---
+++ Week_05/lesson09/beanfactory/bean/School.java
@@ -0,0 +1,15 @@
+package lesson09.beanfactory.bean;
+
+import lombok.Data;
+
+/**
+ * @author Webb Dong
+ * @description: School
+ * @date 2021-02-15 23:01
+ */
+@Data
+public class School {
+
+    private String name;
+
+}
---
+++ Week_05/lesson09/beanfactory/bean/Student.java
@@ -0,0 +1,17 @@
+package lesson09.beanfactory.bean;
+
+import lombok.Data;
+
+/**
+ * @author Webb Dong
+ * @description: Student
+ * @date 2021-02-15 23:00
+ */
+@Data
+public class Student {
+
+    private String name;
+
+    private Integer age;
+
+}
