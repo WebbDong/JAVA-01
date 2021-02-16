c483f130c91c20f76b8376ab5ec4eda2447ca8f9
Webb Dong <wangwebb1991@gmail.com>
2021-02-16 23:21:20 +0800

    Bean Factory Test

--- Week_05/lesson09/beanfactory/BeanFactoryTestMain.java
+++ Week_05/lesson09/beanfactory/BeanFactoryTestMain.java
@@ -1,5 +1,9 @@
 package lesson09.beanfactory;
 
+import lesson09.beanfactory.bean.School;
+import lesson09.beanfactory.context.ApplicationContext;
+import lesson09.beanfactory.context.ClassPathXmlApplicationContext;
+
 /**
  * @author Webb Dong
  * @description: 写代码实现 Spring Bean 的装配
@@ -8,7 +12,9 @@ package lesson09.beanfactory;
 public class BeanFactoryTestMain {
 
     public static void main(String[] args) {
-
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("lesson09/beanfactory/beans.xml");
+//        School school = (School) ac.getBeans("school");
+        System.out.println(Boolean.valueOf(null));
     }
 
 }
