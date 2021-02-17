0c01c7ab913aae85b5868efb4a8f1d520fad0129
Webb Dong <wangwebb1991@gmail.com>
2021-02-18 01:19:30 +0800

    添加 foundingYears 字段

--- Week_05/lesson09/beanfactory/bean/School.java
+++ Week_05/lesson09/beanfactory/bean/School.java
@@ -1,6 +1,8 @@
 package lesson09.beanfactory.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Webb Dong
@@ -8,8 +10,12 @@ import lombok.Data;
  * @date 2021-02-15 23:01
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class School {
 
     private String name;
+
+    private Integer foundingYears;
 
 }
