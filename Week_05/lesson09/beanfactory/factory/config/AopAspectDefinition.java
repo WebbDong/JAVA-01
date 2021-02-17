a13f8eb43d1012906f41949dc8ef97452d408207
Webb Dong <wangwebb1991@gmail.com>
2021-02-18 01:16:01 +0800

    AOP 切面配置定义

---
+++ Week_05/lesson09/beanfactory/factory/config/AopAspectDefinition.java
@@ -0,0 +1,45 @@
+package lesson09.beanfactory.factory.config;
+
+import lesson09.beanfactory.factory.config.enums.AopAdviceTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Webb Dong
+ * @description: AOP 切面配置定义
+ * @date 2021-02-16 21:33
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AopAspectDefinition {
+
+    public static final String REF_ATTRIBUTE_NAME = "ref";
+
+    public static final String POINTCUT_REF_ATTRIBUTE_NAME = "pointcut-ref";
+
+    private String id;
+
+    /**
+     * AOP 切面 bean id
+     */
+    private String ref;
+
+    /**
+     * 目标 bean id
+     */
+    private String targetRef;
+
+    private String pointcutRef;
+
+    private boolean proxyTargetClass;
+
+    private Map<AopAdviceTypeEnum, List<AopAdviceDefinition>> adviceDefMap;
+
+}
