a81f38c139c546a0b442fbfa1e689442e63172c0
Webb Dong <wangwebb1991@gmail.com>
2021-02-18 01:17:24 +0800

    AOP 切点配置定义

--- Week_05/lesson09/beanfactory/factory/config/AopPointcutDefinition.java
+++ Week_05/lesson09/beanfactory/factory/config/AopPointcutDefinition.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class AOPPointcutDefinition {
+public class AopPointcutDefinition {
 
     private String id;
 
