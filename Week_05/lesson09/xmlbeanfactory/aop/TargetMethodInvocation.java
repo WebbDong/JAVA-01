40dd6e02e8db26b1ca7dc87e50b604129e7808d7
Webb Dong <wangwebb1991@gmail.com>
2021-02-19 11:45:04 +0800

    目标方法调用，包含前置通知和后置通知

---
+++ Week_05/lesson09/xmlbeanfactory/aop/TargetMethodInvocation.java
@@ -0,0 +1,15 @@
+package lesson09.xmlbeanfactory.aop;
+
+/**
+ * @author Webb Dong
+ * @description: 目标方法调用，包含前置通知和后置通知
+ * @date 2021-02-19 11:20
+ */
+public class TargetMethodInvocation implements MethodInvocation {
+
+    @Override
+    public Object invoke(Object... args) {
+        return null;
+    }
+
+}
