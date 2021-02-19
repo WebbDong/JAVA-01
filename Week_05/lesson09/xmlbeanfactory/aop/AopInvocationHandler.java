500a7682d1006ea8cd649592e63f309fced9204e
Webb Dong <wangwebb1991@gmail.com>
2021-02-19 11:41:09 +0800

    AopInvocationHandler

---
+++ Week_05/lesson09/xmlbeanfactory/aop/AopInvocationHandler.java
@@ -0,0 +1,28 @@
+package lesson09.xmlbeanfactory.aop;
+
+import lesson09.xmlbeanfactory.factory.config.AopAdviceDefinition;
+import lesson09.xmlbeanfactory.factory.config.enums.AopAdviceTypeEnum;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Webb Dong
+ * @description: AopInvocationHandler
+ * @date 2021-02-14 21:43
+ */
+public class AopInvocationHandler extends AbstractAopAdviceMethodHandler implements InvocationHandler {
+
+    public AopInvocationHandler(Object target, Object aspect,
+                                Map<AopAdviceTypeEnum, List<AopAdviceDefinition>> adviceDefMap) {
+        super(target, aspect, adviceDefMap);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return invokeAdvice(method, args);
+    }
+
+}
