b786039b88aeeac337e58a56af7be54a6deb0a1c
Webb Dong <wangwebb1991@gmail.com>
2021-02-19 21:50:42 +0800

    普通通知方法调用器

---
+++ Week_05/lesson09/xmlbeanfactory/aop/methodinvocation/NormalAdviceMethodInvocation.java
@@ -0,0 +1,18 @@
+package lesson09.xmlbeanfactory.aop.methodinvocation;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author Webb Dong
+ * @description: 普通通知方法调用器
+ * @date 2021-02-19 18:25
+ */
+@SuperBuilder
+public class NormalAdviceMethodInvocation extends AbstractAdviceMethodInvocation {
+
+    @Override
+    public Object invoke(Object... args) throws Throwable {
+        return aspect.getClass().getMethod(invokeMethodName).invoke(aspect);
+    }
+
+}
