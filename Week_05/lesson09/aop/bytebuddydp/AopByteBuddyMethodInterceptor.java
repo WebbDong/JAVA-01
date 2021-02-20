bae4ec99a4c321abb0bfa51f0d1e76afa2b7bb5a
Webb Dong <wangwebb1991@gmail.com>
2021-02-20 17:04:37 +0800

    Byte Buddy 方法拦截器

---
+++ Week_05/lesson09/aop/bytebuddydp/AopByteBuddyMethodInterceptor.java
@@ -0,0 +1,26 @@
+package lesson09.aop.bytebuddydp;
+
+import lesson09.aop.AbstractAopMethodInterceptor;
+import lombok.experimental.SuperBuilder;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.This;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Webb Dong
+ * @description: Byte Buddy 方法拦截器
+ * @date 2021-02-20 11:55
+ */
+@SuperBuilder
+public class AopByteBuddyMethodInterceptor extends AbstractAopMethodInterceptor implements ByteBuddyMethodInterceptor {
+
+    @RuntimeType
+    @Override
+    public Object intercept(@This Object proxy, @Origin Method method, @AllArguments Object[] args) throws Throwable {
+        return invokeAdvice(proxy, method, args);
+    }
+
+}
