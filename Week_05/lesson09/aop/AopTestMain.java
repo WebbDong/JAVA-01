47bcda18ad7c08cb40534c730af4a02a97fd0d97
dongwenbin <dwbdeveloper1991@163.com>
2021-02-20 11:06:00 +0800

    Byte Buddy 方式实现简单 AOP

--- Week_05/lesson09/aop/AopTestMain.java
+++ Week_05/lesson09/aop/AopTestMain.java
@@ -1,5 +1,7 @@
 package lesson09.aop;
 
+import lesson09.aop.bytebuddydp.Cat;
+import lesson09.aop.bytebuddydp.CatAspect;
 import lesson09.aop.cglibdp.AopMethodInterceptor;
 import lesson09.aop.cglibdp.Car;
 import lesson09.aop.cglibdp.CarAspect;
@@ -7,6 +9,9 @@ import lesson09.aop.jdkdp.AopInvocationHandler;
 import lesson09.aop.jdkdp.Person;
 import lesson09.aop.jdkdp.Student;
 import lesson09.aop.jdkdp.StudentAspect;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
 import net.sf.cglib.proxy.Enhancer;
 
 import java.lang.reflect.Proxy;
@@ -40,15 +45,28 @@ public class AopTestMain {
         c.drive();
     }
 
-    private static void aopWithByteBuddy() {
-
+    /**
+     * Byte Buddy 方式实现简单 AOP
+     */
+    private static void aopWithByteBuddy() throws Exception {
+        Cat proxy = new ByteBuddy()
+                .subclass(Cat.class)
+                .method(ElementMatchers.any())
+                .intercept(Advice.to(CatAspect.class))
+                .make()
+                .load(AopTestMain.class.getClassLoader())
+                .getLoaded()
+                .getConstructor()
+                .newInstance();
+        proxy.eat();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         aopWithJavaDynamicProxy();
         System.out.println("--------------------------");
         aopWithCglibDynamicProxy();
         System.out.println("--------------------------");
+        aopWithByteBuddy();
     }
 
 }
