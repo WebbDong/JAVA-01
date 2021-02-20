c276189cd9d75f36068c92ada0974ee473aca941
Webb Dong <wangwebb1991@gmail.com>
2021-02-20 17:13:13 +0800

    ProceedingJoinPoint

--- Week_05/lesson09/aop/ProceedingJoinPoint.java
+++ Week_05/lesson09/aop/ProceedingJoinPoint.java
@@ -9,28 +9,34 @@ public interface ProceedingJoinPoint {
 
     /**
      * 执行目标方法
-     * @return
-     * @throws Throwable
+     * @return 返回目标方法的返回值
+     * @throws Throwable 抛出所有异常
      */
     Object proceed() throws Throwable;
 
     /**
      * 执行目标方法并重新传递参数
-     * @param args
-     * @return
-     * @throws Throwable
+     * @param args 参数
+     * @return 返回目标方法的返回值
+     * @throws Throwable 抛出所有异常
      */
     Object proceed(Object[] args) throws Throwable;
 
     /**
      * 获取被代理的目标对象
-     * @return
+     * @return 返回目标对象
      */
     Object getTarget();
 
     /**
+     * 获取代理对象
+     * @return 返回代理对象
+     */
+    Object getProxy();
+
+    /**
      * 获取参数
-     * @return
+     * @return 返回参数数组
      */
     Object[] getArgs();
 
