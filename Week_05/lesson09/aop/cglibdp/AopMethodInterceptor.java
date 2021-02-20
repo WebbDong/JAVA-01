041af0c7253c23c8f8024426a7a453de1fce9d5a
Webb Dong <wangwebb1991@gmail.com>
2021-02-20 17:07:53 +0800

    AopMethodInterceptor

--- Week_05/lesson09/aop/cglibdp/AopMethodInterceptor.java
+++ Week_05/lesson09/aop/cglibdp/AopMethodInterceptor.java
@@ -1,10 +1,9 @@
 package lesson09.aop.cglibdp;
 
-import lesson09.aop.Aspect;
-import lesson09.aop.ProceedingJoinPoint;
-import lesson09.aop.ProceedingJoinPointImpl;
-import lombok.AllArgsConstructor;
+import lesson09.aop.AbstractAopMethodInterceptor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
@@ -16,21 +15,13 @@ import java.lang.reflect.Method;
  * @date 2021-02-15 14:14
  */
 @Data
-@AllArgsConstructor
-public class AopMethodInterceptor implements MethodInterceptor {
-
-    /**
-     * 目标对象
-     */
-    private Object target;
-
-    private Aspect aspect;
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class AopMethodInterceptor extends AbstractAopMethodInterceptor implements MethodInterceptor {
 
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-        ProceedingJoinPoint joinPoint = new ProceedingJoinPointImpl(args, target, method, aspect);
-        Object ret = aspect.around(joinPoint);
-        return ret;
+        return invokeAdvice(obj, method, args);
     }
 
 }
