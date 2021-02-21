e72720e51038d82fe01da68281924eeb64f16aa1
Webb Dong <wangwebb1991@gmail.com>
2021-02-21 23:27:10 +0800

    XML Bean 工厂

--- Week_05/lesson09/beanfactory/xmlbeanfactory/factory/XmlBeanFactory.java
+++ Week_05/lesson09/beanfactory/xmlbeanfactory/factory/XmlBeanFactory.java
@@ -1,17 +1,14 @@
 package lesson09.beanfactory.xmlbeanfactory.factory;
 
-import lesson09.beanfactory.xmlbeanfactory.factory.config.AopAdviceDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.AopAspectDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.AopPointcutDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.BeanConstructorArgDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.BeanDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.BeanPropertyDefinition;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.enums.AopAdviceTypeEnum;
-import lesson09.beanfactory.xmlbeanfactory.factory.config.enums.BeanScopeTypeEnum;
-import lesson09.beanfactory.xmlbeanfactory.util.AopUtils;
-import lesson09.beanfactory.xmlbeanfactory.util.BeanInitUtils;
-import lesson09.beanfactory.xmlbeanfactory.util.ListUtils;
-import lombok.Getter;
+import lesson09.beanfactory.base.AbstractBeanFactory;
+import lesson09.beanfactory.base.beanfactory.config.AopAdviceDefinition;
+import lesson09.beanfactory.base.beanfactory.config.AopAspectDefinition;
+import lesson09.beanfactory.base.beanfactory.config.AopPointcutDefinition;
+import lesson09.beanfactory.base.beanfactory.config.BeanConstructorArgDefinition;
+import lesson09.beanfactory.base.beanfactory.config.BeanDefinition;
+import lesson09.beanfactory.base.beanfactory.config.BeanPropertyDefinition;
+import lesson09.beanfactory.base.beanfactory.config.enums.AopAdviceTypeEnum;
+import lesson09.beanfactory.base.beanfactory.config.enums.BeanScopeTypeEnum;
 import lombok.SneakyThrows;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -19,10 +16,6 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.InputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +27,7 @@ import java.util.stream.Collectors;
  * @description: XML Bean 工厂
  * @date 2021-02-16 12:00
  */
-@Getter
-public class XmlBeanFactory {
+public class XmlBeanFactory extends AbstractBeanFactory {
 
     public static final String PROXY_TARGET_CLASS_ATTR_NAME = "proxy-target-class";
 
@@ -49,78 +41,8 @@ public class XmlBeanFactory {
 
     private static final String CLASS_ATTRIBUTE_NAME = "class";
 
-    /**
-     * bean map
-     * key: bean id, value: bean 实例
-     */
-    private final Map<String, Object> beanCaching = new HashMap<>();
-
-    /**
-     * aop 代理 bean map
-     * key: aop bean id, value: aop bean 实例
-     */
-    private final Map<String, Object> aopProxyCaching = new HashMap<>();
-
-    /**
-     * bean 配置定义的 map
-     * key: bean id, value: BeanDefinition
-     */
-    private final Map<String, BeanDefinition> beanDefCaching = new HashMap<>();
-
-    /**
-     * 切点配置定义 map
-     * key: pointcut id, value: AOPPointcutDefinition
-     */
-    private final Map<String, AopPointcutDefinition> pointcutDefMap = new HashMap<>();
-
-    /**
-     * aop 切面配置定义 map
-     * key: 目标 bean id, value: AOPAspectDefinition
-     */
-    private final Map<String, AopAspectDefinition> aspectDefMap = new HashMap<>();
-
     public XmlBeanFactory(String configLocation) {
         loadXml(configLocation);
-    }
-
-    /**
-     * 获取 bean
-     * @param name bean 的 id
-     * @return 返回实例
-     */
-    public Object getBean(String name) {
-        BeanDefinition beanDef = beanDefCaching.get(name);
-        if (beanDef.getScopeType() == BeanScopeTypeEnum.SINGLETON) {
-            // 单例模式
-            Object bean = aopProxyCaching.get(name);
-            if (bean == null) {
-                bean = beanCaching.get(name);
-            }
-            return bean;
-        } else {
-            // 原型模式，每次获取都创建新对象
-            return newBean(name);
-        }
-    }
-
-    /**
-     * 创建新的 bean
-     * @param name bean 的 id
-     * @return 返回新的实例
-     */
-    private Object newBean(String name) {
-        BeanDefinition bd = beanDefCaching.get(name);
-        if (bd == null) {
-            return null;
-        }
-
-        Object bean = createAndInitBean(bd);
-        AopAspectDefinition aspectDef = aspectDefMap.get(name);
-        if (aspectDef == null) {
-            return bean;
-        }
-
-        return createAopProxyBean(aspectDef, bean, createAndInitBean(beanDefCaching.get(aspectDef.getRef())));
     }
 
     /**
@@ -169,135 +91,6 @@ public class XmlBeanFactory {
                 .filter(b -> BeanScopeTypeEnum.SINGLETON == b.getScopeType())
                 .collect(Collectors.toList())
                 .forEach(b -> beanCaching.put(b.getId(), createAndInitBean(b)));
-    }
-
-    /**
-     * 初始化所有单例模式的 bean 对应的 AOP 代理的 bean
-     */
-    private void initAllSingletonBeansAopProxyBeans() {
-        aspectDefMap.values()
-                .forEach((ad) -> {
-                    BeanDefinition bd = beanDefCaching.get(ad.getTargetRef());
-                    if (bd == null || bd.getScopeType() != BeanScopeTypeEnum.SINGLETON) {
-                        return;
-                    }
-                    aopProxyCaching.put(ad.getTargetRef(), createAndInitSingletonBeanAopProxyBean(ad));
-                });
-    }
-
-    /**
-     * 创建初始化单例模式的 bean 对应的 AOP 代理的 bean
-     * @param ad 切面定义
-     * @return 返回 AOP 代理对象
-     */
-    private Object createAndInitSingletonBeanAopProxyBean(AopAspectDefinition ad) {
-        Object target = beanCaching.get(ad.getTargetRef());
-        if (target == null) {
-            throw new RuntimeException("target bean not found");
-        }
-        Object aspect = beanCaching.get(ad.getRef());
-        if (aspect == null) {
-            throw new RuntimeException("aspect bean not found");
-        }
-        return createAopProxyBean(ad, target, aspect);
-    }
-
-    /**
-     * 创建 AOP 代理对象
-     * @param ad 切面定义
-     * @param target 目标对象
-     * @param aspect 切面对象
-     * @return 返回 AOP 代理对象
-     */
-    private Object createAopProxyBean(AopAspectDefinition ad, Object target, Object aspect) {
-        Object aopProxy;
-        if (ad.isProxyTargetClass() || target.getClass().getInterfaces().length == 0) {
-            aopProxy = AopUtils.createAOPProxyWithCglib(target, aspect,
-                    target.getClass(),
-                    aspectDefMap.get(ad.getTargetRef()).getAdviceDefMap());
-        } else {
-            aopProxy = AopUtils.createAOPProxyWithJDK(target, aspect,
-                    target.getClass().getInterfaces(),
-                    aspectDefMap.get(ad.getTargetRef()).getAdviceDefMap());
-        }
-        return aopProxy;
-    }
-
-    /**
-     * 创建并初始化 bean
-     * @param b bean 配置定义
-     */
-    @SneakyThrows
-    private Object createAndInitBean(BeanDefinition b) {
-        Class<?> clazz = Class.forName(b.getClassName());
-        List<BeanConstructorArgDefinition> constructorArgDefList = b.getConstructorArgDefList();
-        List<BeanPropertyDefinition> propertyDefList = b.getPropertyDefList();
-        Object bean;
-        if (propertyDefList != null && propertyDefList.size() != 0) {
-            bean = initBeanByProperty(clazz, propertyDefList);
-        } else if (constructorArgDefList != null && constructorArgDefList.size() != 0) {
-            bean = initBeanByConstructor(clazz, constructorArgDefList);
-        } else {
-            bean = clazz.newInstance();
-        }
-        return bean;
-    }
-
-    /**
-     * 根据属性初始化 bean
-     * @param clazz 字节码 class 对象
-     * @param propertyDefList 属性定义集合
-     * @return 返回创建的实例
-     */
-    @SneakyThrows
-    private Object initBeanByProperty(final Class<?> clazz, List<BeanPropertyDefinition> propertyDefList) {
-        Object bean = clazz.newInstance();
-        propertyDefList.forEach(bpd -> {
-            Class<?> beanClass = clazz;
-            while (true) {
-                try {
-                    Field field = beanClass.getDeclaredField(bpd.getName());
-                    field.setAccessible(true);
-                    field.set(bean, BeanInitUtils.getRealTypeValue(field.getType(), bpd.getValue()));
-                    break;
-                } catch (NoSuchFieldException e) {
-                    beanClass = beanClass.getSuperclass();
-                    if (beanClass == Object.class) {
-                        throw new RuntimeException(e);
-                    }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        return bean;
-    }
-
-    /**
-     * 根据构造函数初始化 bean
-     * @param clazz 字节码 class 对象
-     * @param constructorArgDefList 构造器定义集合
-     * @return 返回初始化的实例
-     */
-    @SneakyThrows
-    private Object initBeanByConstructor(Class<?> clazz, List<BeanConstructorArgDefinition> constructorArgDefList) {
-        Constructor<?>[] constructors = clazz.getConstructors();
-        if (constructors.length == 0) {
-            throw new RuntimeException("no constructors");
-        }
-        Constructor<?> constructor = null;
-        for (int i = 0, size = constructorArgDefList.size(); i < constructors.length; i++) {
-            constructor = constructors[i];
-            if (constructor.getParameterCount() == size) {
-                break;
-            }
-        }
-        final Class<?>[] paramTypes = constructor.getParameterTypes();
-        Object[] args = constructorArgDefList.stream()
-                .sorted(Comparator.comparing(BeanConstructorArgDefinition::getIndex))
-                .map(cad -> BeanInitUtils.getRealTypeValue(paramTypes[cad.getIndex()], cad.getValue()))
-                .toArray();
-        return constructor.newInstance(args);
     }
 
     // ---------------------------- Bean 配置相关 -----------------------------
@@ -428,9 +221,11 @@ public class XmlBeanFactory {
     private void initAspectDefinitions(boolean proxyTargetClass, List<Element> elementList) {
         elementList.forEach(e -> {
             String pointcutRef = e.attributeValue(AopAspectDefinition.POINTCUT_REF_ATTRIBUTE_NAME);
+            String id = e.attributeValue(ID_ATTRIBUTE_NAME);
+            String ref = e.attributeValue(AopAspectDefinition.REF_ATTRIBUTE_NAME);
             AopAspectDefinition aspectDef = AopAspectDefinition.builder()
-                    .id(e.attributeValue(ID_ATTRIBUTE_NAME))
-                    .ref(e.attributeValue(AopAspectDefinition.REF_ATTRIBUTE_NAME))
+                    .id(id != null ? id : ref)
+                    .ref(ref)
                     .targetRef(pointcutDefMap.get(pointcutRef).getRef())
                     .pointcutRef(pointcutRef)
                     .proxyTargetClass(proxyTargetClass)
@@ -450,16 +245,9 @@ public class XmlBeanFactory {
      */
     private Map<AopAdviceTypeEnum, List<AopAdviceDefinition>> initAdviceDefinitions(List<Element> elementList) {
         Map<AopAdviceTypeEnum, List<AopAdviceDefinition>> adviceDefMap = new HashMap<>();
-        elementList.forEach(e -> {
-            AopAdviceDefinition adviceDef = AopAdviceDefinition.builder()
-                    .method(e.attributeValue(AopAdviceDefinition.METHOD_ATTRIBUTE_NAME))
-                    .adviceType(AopAdviceTypeEnum.getAdviceTypeEnumByName(e.getName()))
-                    .build();
-            List<AopAdviceDefinition> adviceDefList = adviceDefMap.computeIfAbsent(
-                    adviceDef.getAdviceType(), k -> new ArrayList<>(2));
-            ListUtils.orderedAdd(adviceDefList, adviceDef,
-                    AopAdviceTypeEnum.isAfterTypeAdvice(adviceDef.getAdviceType()));
-        });
+        elementList.forEach(e -> initAdviceDefMap(adviceDefMap,
+                e.attributeValue(AopAdviceDefinition.METHOD_ATTRIBUTE_NAME),
+                AopAdviceTypeEnum.getAdviceTypeEnumByName(e.getName())));
         return adviceDefMap;
     }
 
