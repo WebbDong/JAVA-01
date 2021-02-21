fb3f9da4fe529582638086f8342b97637fec4cb1
Webb Dong <wangwebb1991@gmail.com>
2021-02-21 23:24:17 +0800

    前置通知

--- Week_05/lesson09/beanfactory/annotationbeanfactory/annotation/aop/Before.java
+++ Week_05/lesson09/beanfactory/annotationbeanfactory/annotation/aop/Before.java
@@ -1,9 +1,19 @@
 package lesson09.beanfactory.annotationbeanfactory.annotation.aop;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Webb Dong
  * @description: 前置通知
  * @date 2021-02-14 16:28
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
 public @interface Before {
+
 }
