9fd8cb8c0bbcf85e0c4a880733de6e728e8b1d9c
Webb Dong <wangwebb1991@gmail.com>
2021-02-21 23:26:13 +0800

    切点

--- Week_05/lesson09/beanfactory/annotationbeanfactory/annotation/aop/Pointcut.java
+++ Week_05/lesson09/beanfactory/annotationbeanfactory/annotation/aop/Pointcut.java
@@ -1,12 +1,29 @@
 package lesson09.beanfactory.annotationbeanfactory.annotation.aop;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Webb Dong
  * @description: 切点
  * @date 2021-02-14 16:51
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
 public @interface Pointcut {
 
-    String value();
+    /**
+     * id
+     */
+    String id();
+
+    /**
+     * 需要被代理的目标对象的 bean id
+     */
+    String ref();
 
 }
