588a8d7d3e1a1b1b41cbc6b563cd818a7d9b2114
dongwenbin <dwbdeveloper1991@163.com>
2021-02-22 10:23:29 +0800

    单例的各种写法

--- Week_05/lesson10/singleton/Singleton1.java
+++ Week_05/lesson10/singleton/Singleton1.java
@@ -1,8 +1,9 @@
 package lesson10.singleton;
 
 /**
+ * 单例写法一、饿汉式
+ * 此写法，线程安全，但是不支持延迟加载，存在单例被序列化破坏、克隆破坏和发射破坏
  * @author Webb Dong
- * @description: 单例写法一
  * @date 2021-02-22 00:46
  */
 public class Singleton1 {
