99f72e2c955f6a82e0608967b3152418f1405465
dongwenbin <dwbdeveloper1991@163.com>
2021-02-28 23:55:10 +0800

    Throwables

---
+++ Week_06/src/main/java/lesson11/guava/basicutilities/ThrowablesExample.java
@@ -0,0 +1,13 @@
+package lesson11.guava.basicutilities;
+
+/**
+ * Throwables
+ *      简化异常和错误的传播与检查
+ *
+ *
+ *
+ * @author Webb Dong
+ * @date 2021-02-28 23:53
+ */
+public class ThrowablesExample {
+}
