f1e32f094af0ccbdc2a4dbd440585cb464107150
Webb Dong <wangwebb1991@gmail.com>
2021-03-17 01:51:04 +0800

    将一个2个订单分库，每个库16张订单分表，迁移到4个订单分库，每个库64张订单分表中

--- Week_08/lesson15/sharding-order-scaling-4-64/src/main/java/com/order/scaling/ScalingStart.java
+++ Week_08/lesson15/sharding-order-scaling-4-64/src/main/java/com/order/scaling/ScalingStart.java
@@ -7,25 +7,25 @@ import lombok.SneakyThrows;
 import java.io.InputStream;
 
 /**
- * shardingsphere-4.1.0-sharding-scaling
  * @author Webb Dong
  * @date 2021-03-15 12:27 AM
  */
-public class ScalingStart410 {
+public class ScalingStart {
 
     @SneakyThrows
     public static void main(String[] args) {
-        InputStream in = ScalingStart410.class.getClassLoader()
-                .getResourceAsStream("ScalingSourceConfig-4.1.0.yml");
-        ScalingUtils.startScaling(
+        InputStream in = ScalingStart.class.getClassLoader()
+                .getResourceAsStream("ScalingSourceConfig-4.1.1.yml");
+        ScalingUtils.sendScalingJob(
                 "http://localhost:8888/shardingscaling/job/start",
                 in,
                 DataSourceParameter.builder()
-                        .name("order_sharding_db")
-                        .url("jdbc:mysql://127.0.0.1:3307/order_sharding_db?serverTimezone=UTC&useSSL=false")
+                        .name("order_sharding_4_64_db")
+                        .url("jdbc:mysql://localhost:4000/order_sharding_4_64_db?serverTimezone=UTC&useSSL=false")
                         .username("root")
                         .password("123456")
-                        .build());
+                        .build(),
+                1);
     }
 
 }
